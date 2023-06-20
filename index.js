c45b885ceb09eb10435cbc9d722af945efcfbc8b
Eli Chase <eschase4@gmail.com>
2023-06-20 19:34:52 -0400

    increaded max tokens

--- index.js
+++ index.js
@@ -8,7 +8,7 @@ console.log("Running...")
 async function generateCreepyStory() {
   const data = {
     prompt: 'Write a short horror story for a tweet',
-    max_tokens: 50,
+    max_tokens: 75,
     temperature: 0.7,
     top_p: 1,
     frequency_penalty: 0,
