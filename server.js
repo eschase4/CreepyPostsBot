f0ca9bb64f37090f210693bea691f727401cd610
Eli Chase <eschase4@gmail.com>
2023-06-20 18:44:21 -0400

    heroku5

--- server.js
+++ server.js
@@ -5,7 +5,7 @@ const port = process.env.PORT || 3000; // Use the assigned port from Heroku or a
 require('./index')
 
 app.get('/', (req, res) => {
-  res.send('Hello, world!');
+  res.send("BEEP BOOP BEEP BOOB! I am just a twitter bot! There is nothing to see here!! Feel free to check out my posts and follow me here:  https://twitter.com/CreepyPostBot");
 });
 
 app.listen(port, () => {
