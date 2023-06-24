a88211b677301614e0e65a7e026a020e72173cc7
Eli Chase <eschase4@gmail.com>
2023-06-24 12:36:43 -0400

    raised temperature to 1

--- index.js
+++ index.js
@@ -9,7 +9,7 @@ async function generateCreepyStory() {
   const data = {
     prompt: 'Write a short horror story for a tweet. Get to the ending in under 280 characters.',
     max_tokens: 50,
-    temperature: 0.7,
+    temperature: 1,
     top_p: 1,
     frequency_penalty: 0,
     presence_penalty: 0.6,
