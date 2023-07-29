05d6e2e2c3b391fede3b6674bf005eba73c8bffe
Eli Chase <eschase4@gmail.com>
2023-07-29 17:05:10 -0400

    that one broke it, done expirementing for now

--- server.js
+++ server.js
@@ -5,7 +5,7 @@ const port = process.env.PORT || 3000; // Use the assigned port from Heroku or a
 require('./index')
 
 app.get('/', (req, res) => {
-  res.send("<h1>BEEP BOOP BEEP BOOB!</h1> I am just a twitter bot! There is nothing to see here!! Feel free to check out my posts and follow me here:  https://twitter.com/CreepyPostBot n\ Checkout the source code here: https://github.com/eschase4/CreepyPostsBot" );
+  res.send("BEEP BOOP BEEP BOOB! I am just a twitter bot! There is nothing to see here!! Feel free to check out my posts and follow me here:  https://twitter.com/CreepyPostBot n\ Checkout the source code here: https://github.com/eschase4/CreepyPostsBot" );
 });
 
 app.listen(port, () => {
